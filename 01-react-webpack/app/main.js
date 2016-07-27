f634f72055e5cbc0dabf00bd2e3b19a586f2518b
Deot <zrd0921@qq.com>
2016-07-27 18:06:45 +0800

    react/webpack开发环境搭建
    react/webpack开发环境搭建

---
+++ 01-react-webpack/app/main.js
@@ -0,0 +1,9 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './component.jsx';
+main();
+
+function main() {
+    ReactDOM.render(<Hello />, document.getElementById('app'));
+}
