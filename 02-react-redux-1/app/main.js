1c5b14c8a9fa3f5d26a69d09b197085ace901993
Deot <zrd0921@qq.com>
2016-07-31 22:25:33 +0800

    Redux之旅－1
    Redux之旅－1

---
+++ 02-react-redux-1/app/main.js
@@ -0,0 +1,9 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './redux_lesson/lesson_0';
+main();
+
+function main() {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
